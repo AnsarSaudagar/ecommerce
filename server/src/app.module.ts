e8512e925064f6ddfdf8829af701154bd8d3d639
ansar <ansarsaudagar40@gmail.com>
2024-02-17 23:57:27 +0530

    added basic code for typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'ecommerce',
+      password: '403769',
+      database: 'ecommerce',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
