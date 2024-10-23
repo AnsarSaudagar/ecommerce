c13aa7e0556e162cea4badaa7041ae4751e26883
ansar <ansarsaudagar40@gmail.com>
2024-10-23 23:15:18 +0530

    injected api base url in product categories service

--- src/app/services/product-category.service.ts
+++ src/app/services/product-category.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductCategoryService {
+  constructor(
+    private http: HttpClient,
+    @Inject('API_BASE_URL') private apiUrl: string
+  ) {}
 
-  constructor(private http: HttpClient) {
-
-   }
-
-  getProductCategories() : any{
-    return this.http.get<any>(environment.backendUrl + "product-categories");
+  getProductCategories(): any {
+    return this.http.get<any>(this.apiUrl + 'product-categories');
   }
-
 }
