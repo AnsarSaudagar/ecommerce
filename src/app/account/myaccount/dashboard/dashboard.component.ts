7b72c4749f1af7ace98244e5d2a4af8470c9685b
ansar <ansarsaudagar40@gmail.com>
2024-11-16 01:33:03 +0530

    user service created and data displayed on profile

--- src/app/account/myaccount/dashboard/dashboard.component.ts
+++ src/app/account/myaccount/dashboard/dashboard.component.ts
@@ -4,15 +4,18 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { UserDataService } from 'src/app/auth-old/user-data.service';
 import { AuthService } from 'src/app/auth-old/auth.service';
 import { ModalService } from './modal.service';
+import { UserService } from 'src/app/auth/user.service';
+import { User } from 'src/app/auth/user';
+import { UserModel } from 'src/app/models/user.model';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  faTick = faCheckCircle
-  show: any = false
-  localData = JSON.parse(localStorage.getItem('userData'))
+  faTick = faCheckCircle;
+  show: any = false;
+  localData = JSON.parse(localStorage.getItem('userData'));
   email = this.localData.email;
   data = {
     email: '',
@@ -20,57 +23,66 @@ export class DashboardComponent implements OnInit {
     lastName: '',
     password: '',
     username: '',
-    key: ''
+    key: '',
   };
-  modalTopic = ""
+  modalTopic = '';
   showSpinner = true;
+
+  // new 
+
+  userData : UserModel;
+  fullName: string;
 
   constructor(
     private userDataService: UserDataService,
     private auth: AuthService,
-    private modalService: ModalService) { }
+    private modalService: ModalService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-
-    this.userDataService.getSpecificUserData(this.email).subscribe(data => {
-      this.data = data;
-      this.showSpinner = false;
-    })
+    this.userService.getUserProfile().subscribe({
+      next: (user: UserModel) => {
+        this.userData = user;
+        this.fullName = user.first_name + " " + user.last_name;
+      },
+      complete: () => {
+        this.showSpinner = false;
+      },
+    });
   }
-
 
   showModal() {
     if (!this.show) {
-      return true
-    }
-    else {
-      return false
+      return true;
+    } else {
+      return false;
     }
   }
   clickUpdate(name: string) {
-    this.show = true
-    this.modalTopic = name
-    this.getModalTopic(name)
-    this.modalService.modalTopic.next(name)
+    this.show = true;
+    this.modalTopic = name;
+    this.getModalTopic(name);
+    this.modalService.modalTopic.next(name);
   }
 
   closeModal() {
-    this.show = false
-    console.log("checking");
-    this.userDataService.getSpecificUserData(this.email).subscribe(data => {
+    this.show = false;
+    console.log('checking');
+    this.userDataService.getSpecificUserData(this.email).subscribe((data) => {
       this.data = data;
-    })
-
+    });
   }
 
   onDeleteAccount() {
-    this.auth.deleteAccount(this.localData._token, this.data.key, this.data.username)
+    this.auth.deleteAccount(
+      this.localData._token,
+      this.data.key,
+      this.data.username
+    );
   }
 
   getModalTopic(topic: string) {
-    this.modalTopic = topic
+    this.modalTopic = topic;
   }
-
 }
-
-
