dc2aa93c31f30f774b8ec21827445287aefc4f82
ansar <ansarsaudagar40@gmail.com>
2024-11-17 15:31:33 +0530

    Interceptor document formatted

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,33 +1,37 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpParams, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, exhaustMap, take } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthenticationService) {}
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
 
-    constructor(private authService: AuthenticationService) { }
-    intercept(req: HttpRequest<any>, next: HttpHandler) {
-        
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                if (!user) {
-                    return next.handle(req)
-                }
-
-                const modifiedReq = req.clone({
-                    headers: new HttpHeaders({
-                        'Authorization': `Bearer ${user.token}`,
-                        'Content-Type': 'application/json',
-                    }),
-                    params: new HttpParams().set('auth', user.token)
-                })
-                return next.handle(modifiedReq);
-            })
-        )
-    }
+        const modifiedReq = req.clone({
+          headers: new HttpHeaders({
+            Authorization: `Bearer ${user.token}`,
+            'Content-Type': 'application/json',
+          }),
+          params: new HttpParams().set('auth', user.token),
+        });
+        return next.handle(modifiedReq);
+      })
+    );
+  }
 }
