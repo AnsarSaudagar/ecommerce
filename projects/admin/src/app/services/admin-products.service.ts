932e81d50263f55ff248c2aab4f3ac2558f14835
ansar <ansarsaudagar40@gmail.com>
2025-10-23 00:00:22 +0530

    java url handled

--- projects/admin/src/app/services/admin-products.service.ts
+++ projects/admin/src/app/services/admin-products.service.ts
@@ -8,39 +8,39 @@ import { environment } from 'projects/ecommerce/src/environments/environment.dev
   providedIn: 'root'
 })
 export class AdminProductsService {
-  private apiUrl = environment.backendNodeUrl;
+  private apiUrl = environment.backendJavaUrl + "admin";
 
   constructor(private http: HttpClient) {}
 
   // Get all products
   getAllProducts(): Observable<ProductModel[]> {
-    return this.http.get<ProductModel[]>(`${this.apiUrl}products`);
+    return this.http.get<ProductModel[]>(`${this.apiUrl}/products`);
   }
 
   // Get single product
   getProduct(id: number): Observable<ProductModel> {
-    return this.http.get<ProductModel>(`${this.apiUrl}products/${id}`);
+    return this.http.get<ProductModel>(`${this.apiUrl}/products/${id}`);
   }
 
   // Create new product
   createProduct(product: FormData): Observable<ProductModel> {
-    return this.http.post<ProductModel>(`${this.apiUrl}products`, product);
+    return this.http.post<ProductModel>(`${this.apiUrl}/products`, product);
   }
 
   // Update product
   updateProduct(id: number, product: FormData): Observable<ProductModel> {
-    return this.http.put<ProductModel>(`${this.apiUrl}products/${id}`, product);
+    return this.http.put<ProductModel>(`${this.apiUrl}/products/${id}`, product);
   }
 
   // Delete product
   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}products/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/products/${id}`);
   }
 
   // Upload product image
   uploadProductImage(file: File): Observable<{url: string}> {
     const formData = new FormData();
     formData.append('image', file);
-    return this.http.post<{url: string}>(`${this.apiUrl}products/upload-image`, formData);
+    return this.http.post<{url: string}>(`${this.apiUrl}/products/upload-image`, formData);
   }
 }
